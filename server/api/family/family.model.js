6b824f5795d42389d3bd7285946714864c9462d9
Lucas Assmann <l.assmann.30@gmail.com>
2017-04-24 19:53:24 -0300

    added fields to models

--- server/api/family/family.model.js
+++ server/api/family/family.model.js
@@ -1,13 +1,15 @@
 'use strict';
 
 import mongoose from 'mongoose';
-import {registerEvents} from './family.events';
+import { registerEvents } from './family.events';
 
 var FamilySchema = new mongoose.Schema( {
   surname: { type: String, required: true },
-  members: [ mongoose.Schema.Types.ObjectId ],
-  expenseTypes: [String]
+  members: [ { type: mongoose.Schema.Types.ObjectId, Ref: 'User' } ],
+  owner: [ { type: mongoose.Schema.Types.ObjectId, Ref: 'User' } ],
+  expenseTypes: [ String ],
+  image: String
 } );
 
-registerEvents(FamilySchema);
-export default mongoose.model('Family', FamilySchema);
+registerEvents( FamilySchema );
+export default mongoose.model( 'Family', FamilySchema );
--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,6 @@ export default function(app) {
   // Insert routes below
   app.use('/api/family', require('./api/family'));
   app.use('/api/expenses', require('./api/expense'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth').default);
