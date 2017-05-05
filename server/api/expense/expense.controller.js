0102fa61a912b87c388b2c33bb9c26c43281b43b
Lucas Assmann <l.assmann.30@gmail.com>
2017-05-05 19:42:19 -0300

    get all expenses by userId

--- server/api/expense/expense.controller.js
+++ server/api/expense/expense.controller.js
@@ -11,77 +11,76 @@
 'use strict';
 
 import jsonpatch from 'fast-json-patch';
-import {Expense, ExpenseType} from './expense.model';
+import { Expense, ExpenseType } from './expense.model';
 
 
-function respondWithResult(res, statusCode) {
+function respondWithResult( res, statusCode ) {
   statusCode = statusCode || 200;
-  return function(entity) {
-    if(entity) {
-      return res.status(statusCode).json(entity);
+  return function ( entity ) {
+    if (entity) {
+      return res.status( statusCode ).json( entity );
     }
     return null;
   };
 }
 
-function patchUpdates(patches) {
-  return function(entity) {
+function patchUpdates( patches ) {
+  return function ( entity ) {
     try {
       // eslint-disable-next-line prefer-reflect
-      jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
-      return Promise.reject(err);
+      jsonpatch.apply( entity, patches, /*validate*/ true );
+    } catch (err) {
+      return Promise.reject( err );
     }
 
     return entity.save();
   };
 }
 
-function removeEntity(res) {
-  return function(entity) {
-    if(entity) {
+function removeEntity( res ) {
+  return function ( entity ) {
+    if (entity) {
       return entity.remove()
-        .then(() => {
-          res.status(204).end();
-        });
+        .then( () => {
+          res.status( 204 ).end();
+        } );
     }
   };
 }
 
-function handleEntityNotFound(res) {
-  return function(entity) {
-    if(!entity) {
-      res.status(404).end();
+function handleEntityNotFound( res ) {
+  return function ( entity ) {
+    if (!entity) {
+      res.status( 404 ).end();
       return null;
     }
     return entity;
   };
 }
 
-function handleError(res, statusCode) {
+function handleError( res, statusCode ) {
   statusCode = statusCode || 500;
-  return function(err) {
-    res.status(statusCode).send(err);
+  return function ( err ) {
+    res.status( statusCode ).send( err );
   };
 }
 
 // Gets a list of Expenses
-export function index(req, res) {
-  return Expense.find(
-    {
-      "_id":req.user._id
+export function index( req, res ) {
+  return Expense.find( {
+      "userId": req.user._id
     }
-  ).sort('-expenseDate').exec()
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+  ).sort( '-expenseDate' ).exec()
+    .then( respondWithResult( res ) )
+    .catch( handleError( res ) );
 }
 
 // Gets a single Expense from the DB
-export function show(req, res) {
-  return Expense.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+export function show( req, res ) {
+  return Expense.findById( req.params.id ).exec()
+    .then( handleEntityNotFound( res ) )
+    .then( respondWithResult( res ) )
+    .catch( handleError( res ) );
 }
 
 /**
@@ -89,71 +88,76 @@ export function show(req, res) {
  * @param req
  * @param res
  */
-export function showExpenseTypes(req, res) {
-  return ExpenseType.find({}).exec()
-    .then(handleEntityNotFound(res))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+export function showExpenseTypes( req, res ) {
+  return ExpenseType.find( {} ).exec()
+    .then( handleEntityNotFound( res ) )
+    .then( respondWithResult( res ) )
+    .catch( handleError( res ) );
 }
 
 // Creates a new Expense in the DB
-export function create(req, res) {
+export function create( req, res ) {
   req.body.userId = req.user && req.user._id;
-  return Expense.create(req.body)
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
+  return Expense.create( req.body )
+    .then( respondWithResult( res, 201 ) )
+    .catch( handleError( res ) );
 }
 
-export function createExpenseType (req, res, nex){
+export function createExpenseType( req, res, nex ) {
   req.body.userId = req.user._id;
-  return ExpenseType.create(req.body)
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
+  return ExpenseType.create( req.body )
+    .then( respondWithResult( res, 201 ) )
+    .catch( handleError( res ) );
 }
 
 // Upserts the given Expense in the DB at the specified ID
-export function upsert(req, res) {
-  if(req.body._id) {
-    Reflect.deleteProperty(req.body, '_id');
+export function upsert( req, res ) {
+  if (req.body._id) {
+    Reflect.deleteProperty( req.body, '_id' );
   }
-  return Expense.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
+  return Expense.findOneAndUpdate( { _id: req.params.id }, req.body, {
+    new: true,
+    upsert: true,
+    setDefaultsOnInsert: true,
+    runValidators: true
+  } ).exec()
 
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    .then( respondWithResult( res ) )
+    .catch( handleError( res ) );
 }
 
 // Updates an existing Expense in the DB
-export function patch(req, res) {
-  if(req.body._id) {
-    Reflect.deleteProperty(req.body, '_id');
+export function patch( req, res ) {
+  if (req.body._id) {
+    Reflect.deleteProperty( req.body, '_id' );
   }
-  return Expense.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(patchUpdates(req.body))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+  return Expense.findById( req.params.id ).exec()
+    .then( handleEntityNotFound( res ) )
+    .then( patchUpdates( req.body ) )
+    .then( respondWithResult( res ) )
+    .catch( handleError( res ) );
 }
 
 // Deletes a Expense from the DB. Only a user can delete it's expenses
-export function destroy(req, res) {
-  return Expense.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
-    .catch(handleError(res));
+export function destroy( req, res ) {
+  return Expense.findById( req.params.id ).exec()
+    .then( handleEntityNotFound( res ) )
+    .then( removeEntity( res ) )
+    .catch( handleError( res ) );
 }
 
-export function getExpenses(req, res) {
-  return Expense.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
-    .catch(handleError(res));
+export function getExpenses( req, res ) {
+  return Expense.findById( req.params.id ).exec()
+    .then( handleEntityNotFound( res ) )
+    .then( removeEntity( res ) )
+    .catch( handleError( res ) );
 }
 
 /**
  * Return all the expenses by date and ordered
  */
-export function getByDate (req, res){
-  return Expense.find({_id:req.user._id}).exec()
+export function getByDate( req, res ) {
+  return Expense.find( { _id: req.user._id } ).exec()
     .then()
 
 }
